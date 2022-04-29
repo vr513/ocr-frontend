51dfd69bd8f7cadd7a9507b1e3689fbd15c25aad
Varad Rajopadhye <varadmanraj@gmail.com>
2022-04-29 09:31:28 +0530

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Head from "next/head";
+
+const baseUrl = "http://localhost:8080/get-text";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export { baseUrl };
+export default MyApp;
---
+++ pages/convert.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from "react";
+import Head from "next/dist/shared/lib/head";
+import { baseUrl } from "./_app";
+import styles from '../styles/Convert.module.css'
+
+
+function convert() {
+  const [text, setText] = useState(null);
+  const [fileSrc, setFileSrc] = useState(null);
+  const form = useRef(null);
+
+  function handleImageChange(evt) {
+    setFileSrc(URL.createObjectURL(evt.target.files[0]))
+    console.log(fileSrc);
+  }
+
+  async function getText(evt) {
+    evt.preventDefault();
+    const data = new FormData(form.current)
+    setText("Please wait while we fetch your result")
+    let res = await fetch(baseUrl, {
+      method: "POST",
+      body: data
+    })
+    res = await res.json();
+    console.log(res)
+    setText(res.result)
+  }
+  return <>
+    <div className="container">
+      <h2 className={styles.mainHead}>Optical Character Recognition</h2>
+      <div className={styles.main}>
+        <form ref={form} onSubmit={getText} className={styles.formMain}>
+
+          <div className="form-group">
+            <h2 className={styles.formHead} htmlFor="exampleFormControlFile1">Test Image input</h2><br />
+            <input type="file" name="file" className={styles.formImageInput} onChange={handleImageChange} id="exampleFormControlFile1" /><br />
+          </div>
+          <img src={fileSrc} alt="" className={styles.selectedImage} />
+          <button type="submit" className={styles.btnSubmit}>Submit</button>
+          {text ? (
+            <>
+              <h2 className={styles.formHead}>Result</h2>
+              <p>{text}</p>
+            </>
+          ) : <></>}
+        </form>
+      </div>
+    </div>
+
+  </>;
+}
+
+export default convert;
